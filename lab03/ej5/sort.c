466e9c10333559fff45fe18a893d1c44d9dfabef
Federico Virgolini <fvirgolini@gmail.com>
2021-04-20 10:19:11 -0300

    ej 5 resolved

--- lab03/ej5/sort.c
+++ lab03/ej5/sort.c
@@ -11,9 +11,47 @@
 #include "sort.h"
 #include "player.h"
 
+void swap(player_t *p, unsigned int i, unsigned int j) {
+    player_t aux = *(p+i);
+    *(p+i) = *(p+j);
+    *(p+j) = aux;
+}
+
+static unsigned int partition(player_t a[], unsigned int izq, unsigned int der) {
+    unsigned int ppiv = izq;
+    unsigned int indice_izq = izq + 1;
+    unsigned int indice_der = der;
+    player_t *p = NULL;
+    p = &a[0];   
+    while (indice_izq <= indice_der) {
+        if (goes_before(a[indice_izq], a[ppiv])) {
+            ++indice_izq;
+        } else if (goes_before(a[ppiv], a[indice_der])) {
+            --indice_der;
+        } else if (goes_before(a[ppiv], a[indice_izq]) && goes_before(a[indice_der], a[ppiv])) {
+            swap(p, indice_izq, indice_der);
+        }
+    }
+    swap(p, ppiv, indice_der);
+    return indice_der;
+}
+
+static void quick_sort_rec(player_t a[], unsigned int izq, unsigned int der) {
+    unsigned int ppiv = 0u;
+    if(izq < der) {
+    
+        ppiv = partition(a, izq, der);
+        
+        if (ppiv > 0) {
+            quick_sort_rec(a, izq, ppiv-1);
+        }
+        quick_sort_rec(a, ppiv+1, der);
+    }
+}
+
 bool goes_before(player_t x, player_t y){
-    // completar aquí
-    return true;
+    bool goes_before = (x->rank <= y->rank);
+    return goes_before;
 }
 
 bool array_is_sorted(player_t atp[], unsigned int length) {
@@ -25,6 +63,8 @@ bool array_is_sorted(player_t atp[], unsigned int length) {
 }
 
 void sort(player_t a[], unsigned int length) {
-    // completar aquí
+    if (length > 0) {
+        quick_sort_rec(a, 0, length - 1);
+    }
 }
 
