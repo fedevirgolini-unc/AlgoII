290fa3b345e9512eb40d2f2c08abce051f6daeb2
Federico Virgolini <fvirgolini@gmail.com>
2021-04-20 10:03:51 -0300

    ej4 resolved

--- lab03/ej4/proc1.c
+++ lab03/ej4/proc1.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
 void absolute(int x, int y) {
-    // Completar aqui
+    if (x >= 0) {
+        y = x;
+    } else {
+        y = -x;
+    }
 }
 
 int main(void) {
-    // Completar aqui
+    
+    int a, res;
+    res = 0;
+    a = -98;
+    
+    absolute(a, res);
+    
+    printf("%d\n", res);
+    
     return 0;
 }
 
--- lab03/ej4/proc2.c
+++ lab03/ej4/proc2.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
 void absolute(int x, int *y) {
-    // Completar aqui
+    if (x >= 0) {
+        *y = x;
+    } else {
+        *y = -x;
+    }
 }
 
 int main(void) {
-    // Completar aqui
+    int a, res;
+    int *p = NULL;
+    
+    res = 0;
+    a = -98;
+    p = &res;
+    
+    absolute(a, p);
+    
+    printf("%d\n", res);
+    
+    return 0;
     return 0;
 }
 
